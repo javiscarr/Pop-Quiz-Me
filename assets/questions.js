2f32f6e9fb1b784255be0ddb479ee852b3816633
Doonywoony <javismcarr@gmail.com>
2021-12-03 11:55:47 -0500

    EventListeners added for functions and clicks

--- assets/questions.js
+++ assets/questions.js
@@ -1,31 +1,36 @@
- // variables for page elements
+// variables for page elements
 // time and score
 let timeEl = document.querySelector("p.time");
 let secondsLeft = 75;
 let scoreEl = document.querySelector("#score");
 
-// sections
 // section intro
 const introEl = document.querySelector("#intro");
 
-// section questions
 //question section
 const questionsEl = document.querySelector("#questions");
-//where question goes
+
+//question area
 let questionEl = document.querySelector("#question");
-// how many questions they have answered
+
+// questions answered
 let questionCount = 0;
-// div yaynay
+
+// div rightWrong
 const rightWrongEl = document.querySelector("#rightWrong");
 
 // section final
 const finalEl = document.querySelector("#final");
+
 // user initials
 let initialsInput = document.querySelector("#initials");
+
 // section highscores
 const highscoresEl = document.querySelector("#highscores");
+
 // ordered list
 let scoreListEl = document.querySelector("#score-list");
+
 // array of scores
 let scoreList = [];
 
@@ -33,7 +38,7 @@ let scoreList = [];
 // start
 const startBtn = document.querySelector("#start");
 // answer button class
-const ansBtn = document.querySelectorAll("button.ansBtn")
+const ansBtn = document.querySelectorAll("button.ansBtn");
 // answer1
 const ans1Btn = document.querySelector("#answer1");
 // answer2
@@ -45,97 +50,232 @@ const ans4Btn = document.querySelector("#answer4");
 // submit-score
 const submitScrBtn = document.querySelector("#submit-score");
 // goback
-const goBackBtn = document.querySelector("#goback");
+const backBtn = document.querySelector("#back");
 // clearscores
 const clearScrBtn = document.querySelector("#clearscores");
 // view-scores
 const viewScrBtn = document.querySelector("#view-scores");
 
- //object array
- const questions = [ 
-{
-    
-       question: "What does HTML stand for?",
-       choices: 
-            ["HyperText MarkUp Language",
-            "HyperTension Marking Language",
-            "HyperText MakeUp Language",
-            "HyperText MarkUp Lagoon"],
+//object array
+const questions = [
+  {
+    question: "What does HTML stand for?",
+    choices: [
+      "HyperText MarkUp Language",
+      "HyperTension Marking Language",
+      "HyperText MakeUp Language",
+      "HyperText MarkUp Lagoon",
+    ],
 
-            
-        answer: "HyperText MarkUp Language",
-    },
+    answer: "HyperText MarkUp Language",
+  },
 
-{
-    
-       question: "What data type does var x represent? x = true",
-       choices: ["String", "Bit", "Boolean", "Object"],
-       answer: "Boolean",
+  {
+    question: "What data type does var x represent? x = true",
+    choices: ["String", "Bit", "Boolean", "Object"],
+    answer: "Boolean",
+  },
 
-    },
+  {
+    question: "What is a Cascading Spread Sheet (CSS)?",
+    choices: [
+      "A language of status rules that allows the developer to change the content of HTML",
+      "A scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else",
+      "A language of style rules that we use to apply styling to our HTML content",
+      "A language that we use to structure and give meaning to our web content",
+    ],
 
-{
+    answer:
+      "A language of style rules that we use to apply styling to our HTML content",
+  },
 
-        question: "What is a Cascading Spread Sheet (CSS)?",
-        choices: [
-            "A language of status rules that allows the developer to change the content of HTML",
-            "A scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else",
-            "A language of style rules that we use to apply styling to our HTML content",
-            "A language that we use to structure and give meaning to our web content"],
-    
-        answer: "A language of style rules that we use to apply styling to our HTML content",
-    
-    },
+  {
+    question:
+      "Which function should we call after a submit event to prevent data loss?",
+    choices: [
+      "event.onLoad();",
+      "event.preventRefresh();",
+      "event.stopLoad();",
+      "event.preventDefault();",
+    ],
+    answer: "event.preventDefault();",
+  },
 
-{
-        question:
-          "Which function should we call after a submit event to prevent data loss?",
-        choices: [
-          "event.onLoad();",
-          "event.preventRefresh();",
-          "event.stopLoad();",
-          "event.preventDefault();"
-        ],
-        answer: "event.preventDefault();",
-    },
-    
-{
+  {
+    question:
+      "Which expression is the most useful and recommended debugging tool?",
+    choices: ["alert(x);", "confirm(x);", "console.log(x);", "prompt(x);"],
+    answer: "console.log(x);",
+  },
 
-     
-        question: "Which expression is the most useful and recommended debugging tool?",
-        choices: ["alert(x);", "confirm(x);", "console.log(x);", "prompt(x);"],
-        answer: "console.log(x);",
-      
-    },
-
-{
-     
-        question: "What is Document Object Model (DOM)?",
-        choices : [
-            "The data representation of the objects that comprise the structure and content of a document on the web",
-            "Read-only property of the document interface returns the document location as a string.",
-            "Returns the element that is the root element of the document",
-            "Abstract interface represents a Node object that contains characters. "
-        ],
-        answer: "The data representation of the objects that comprise the structure and content of a document on the web",
-    
-    },
-
+  {
+    question: "What is Document Object Model (DOM)?",
+    choices: [
+      "The data representation of the objects that comprise the structure and content of a document on the web",
+      "Read-only property of the document interface returns the document location as a string.",
+      "Returns the element that is the root element of the document",
+      "Abstract interface represents a Node object that contains characters. ",
+    ],
+    answer:
+      "The data representation of the objects that comprise the structure and content of a document on the web",
+  },
 ];
 
 // Functions
 
 // timer
 function setTime() {
-    let timerInterval = setInterval(function () {
-        secondsLeft--;
-        timeEl.textContent = `Time:${secondsLeft}s`;
+  let timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = `Time:${secondsLeft}s`;
 
-        if (secondsLeft === 0 || questionCount === questions.length) {
-            clearInterval(timerInterval);
-            questionsEl.style.display = "none";
-            finalEl.style.display = "block";
-            scoreEl.textContent = secondsLeft;
-        }
-    }, 1000);
+    if (secondsLeft === 0 || questionCount === questions.length) {
+      clearInterval(timerInterval);
+      questionsEl.style.display = "none";
+      finalEl.style.display = "block";
+      scoreEl.textContent = secondsLeft;
+    }
+  }, 1000);
 }
+// start quiz with timer and set up questions
+function startQuiz() {
+  introEl.style.display = "none";
+  questionsEl.style.display = "block";
+  questionCount = 0;
+
+  setTime();
+  setQuestion(questionCount);
+}
+
+// function to set question; takes in a count and displays the next question/answers
+function setQuestion(id) {
+  if (id < questions.length) {
+    questionEl.textContent = questions[id].question;
+    ans1Btn.textContent = questions[id].choices[0];
+    ans2Btn.textContent = questions[id].choices[1];
+    ans3Btn.textContent = questions[id].choices[2];
+    ans4Btn.textContent = questions[id].choices[3];
+  }
+}
+
+// function to check answer and then move to next question
+function answerCheck(event) {
+  event.preventDefault();
+
+  // show section for rightWrong and append message
+  rightWrongEl.style.display = "block";
+  let p = document.createElement("p");
+  rightWrongEl.appendChild(p);
+
+  // time out after 1 second
+  setTimeout(function () {
+    p.style.display = "none";
+  }, 1000);
+
+  // answer checker
+  if (questions[questionCount].answer === event.target.value) {
+    p.textContent = "Correct!";
+  } else if (questions[questionCount].answer !== event.target.value) {
+    secondsLeft = secondsLeft - 10;
+    p.textContent = "Wrong!";
+  }
+
+  // increment so the questions index is increased
+  if (questionCount < questions.length) {
+    questionCount++;
+  }
+  // call setQuestion to bring in next question when any ansBtn is clicked
+  setQuestion(questionCount);
+}
+
+
+
+function addScore(event) {
+    event.preventDefault();
+
+    finalEl.style.display = "none";
+    highscoresEl.style.display = "block";
+
+    let init = initialsInput.value.toUpperCase();
+    scoreList.push({ initials: init, score: secondsLeft });
+
+    // sort scores
+    scoreList = scoreList.sort((a, b) => {
+        if (a.score < b.score) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+
+      scoreListEl.innerHTML="";
+    for (let i = 0; i < scoreList.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = `${scoreList[i].initials}: ${scoreList[i].score}`;
+        scoreListEl.append(li);
+    }
+
+    // Add to local storage
+    storeScores();
+    displayScores();
+}
+
+function storeScores() {
+    localStorage.setItem("scoreList", JSON.stringify(scoreList));
+
+}
+
+function displayScores() {
+    // retrieve scores from localStorage
+    // this will parse the string to an object
+    let storedScoreList = JSON.parse(localStorage.getItem("scoreList"));
+
+    // If there were any scores captured in localStorage then update the score array list
+    if (storedScoreList !== null) {
+        scoreList = storedScoreList;
+    }
+}
+
+// clear stored scores
+function clearScores() {
+    localStorage.clear();
+    scoreListEl.innerHTML="";
+
+}
+
+// EventListeners added to functions
+
+// this will display the first question and start the timer when the start quiz button is clicked
+startBtn.addEventListener("click", startQuiz);
+
+// loop for the correct answer
+ansBtn.forEach(item => {
+    item.addEventListener('click', answerCheck);
+});
+
+// score is added
+submitScrBtn.addEventListener("click", addScore);
+
+// button to go backwards
+backBtn.addEventListener("click", function (){
+    highscoresEl.style.display = "none";
+    introEl.style.display = "block";
+    secondsLeft = 75;
+    timeEl.textContent = `Time:${secondsLeft}s`;
+});
+
+// scores are cleared
+clearScrBtn.addEventListener("click", clearScores);
+
+// button to view or hide scores
+viewScrBtn.addEventListener("click", function(){
+    if (highscoresEl.style.display === "none"){
+        highscoresEl.style.display = "block";
+    } 
+    else if (highscoresEl.style.display === "block"){
+        highscoresEl.style.display = "none";
+    }
+    else {
+        return alert ("There are no scores to show!");
+    }
+});
